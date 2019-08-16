c5762d2a8d35de730f05f0737e57cac657f18f85
orrlevinger <mr.levinger@gmail.com>
2019-08-16 13:59:08 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+const orgsToRulesMap = new Map([
+    ['NIKE',new Map(
+        [['Paynow', transaction => {
+        return transaction;
+    }],
+    ])]
+]);
+
+
+app.use(bodyParser.json());
+app.post('/:customername/transaction', async (req, res, next) => {
+    try {
+        const {body: transaction} = req;
+        const {customername: customerName} = req.params;
+        const result = orgsToRulesMap.get(customerName).get(transaction.type)(transaction);
+        await fs.writeFile("test.json", JSON.stringify(req.body), function (err) {
+            if (err) {
+                res.status(404).json({
+                    message: 'Failed to parse request',
+                    more_info: err,
+                });
+            }
+
+            res.status(201).send(result);
+
+        });
+    } catch (e) {
+        res.status(500).json({
+            message: 'Failed to parse request',
+            more_info: e,
+        });
+    }
+})
+
+
+app.get('/:customername/balance', (req, res) => {
+
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
